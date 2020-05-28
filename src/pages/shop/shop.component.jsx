cea469a54d11a454156a3ad7629dd6ea2b5fe1de
Adam Gillies <adam.d.gillies@gmail.com>
2020-05-28 22:16:38 +0100

    Move directory and shop data into reducers and implemented selectors. Move collection-preview to collection overview.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview></CollectionsOverview>
+    </div>
+  );
+};
 
 export default ShopPage;
